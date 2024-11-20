458400013ddf8add32f46d66373d90593b6be65e
Alex Abatov <a.aliksant315@gmail.com>
2024-11-20 09:12:21 +0900

    Filtering function added

--- js/app.js
+++ js/app.js
@@ -51,6 +51,13 @@ class CalorieTracker{
         }
     }
 
+    reset(){
+        this._totalCalories = 0
+        this._meals = []
+        this._workouts = []
+        this._render()
+    }
+
     //Private methods
     _displayCaloriesTotal(){
         const totalCaloriesEl = document.getElementById('calories-total')
@@ -185,12 +192,18 @@ class Workout{
 class App{
     constructor(){
         this._tracker = new CalorieTracker()
+        //Event Listeners
 
         document.getElementById('meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'))
         document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'))
 
         document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'))
         document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'))
+
+        document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'))
+        document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'))
+
+        document.getElementById('reset').addEventListener('click', this._reset.bind(this))
     }
 
     _newItem(type, e){
@@ -232,15 +245,33 @@ class App{
                     type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id)
 
                     e.target.closest('.card').remove() //remove div card in which meal was written
-                    
-                    
                 }
-                
-                
             }
         }
+        _filterItems(type, e){
+            const text = e.target.value.toLowerCase()
+            document.querySelectorAll(`#${type}-items .card`).forEach(item =>{
+                const name = item.firstElementChild.firstElementChild.textContent
+                if (name.toLowerCase().indexOf(text) !== -1) {
+                    item.style.display = 'block'
+                }
+                else{
+                    item.style.display = 'none'
+                }
+            })
+            
 
+        }
 
+        _reset(){
+            this._tracker.reset()
+
+            document.getElementById('meal-items').innerHTML = ''
+            document.getElementById('workout-items').innerHTML = ''
+            document.getElementById('filter-meals').value = ''
+            document.getElementById('filter-items').value = ''
+
+        }
 }
 const app = new App()
 
